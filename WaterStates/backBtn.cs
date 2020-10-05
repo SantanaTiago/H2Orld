f232b92934aa871b2b3d9d70c81aac467cafffa3
SantanaTiago <54704492+SantanaTiago@users.noreply.github.com>
2020-10-05 14:30:12 +0100

    Add files via upload

---
+++ WaterStates/backBtn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backBtn : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Loading Menu...");
+    }
+}
