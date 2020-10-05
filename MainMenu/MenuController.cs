9b784f48e5a7abf7fe4ae81d9c7ee90d586e7442
SantanaTiago <54704492+SantanaTiago@users.noreply.github.com>
2020-10-05 14:18:38 +0100

    Add files via upload

---
+++ MainMenu/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene("MainGame");
+        Debug.Log("Loading MainGame...");
+    }
+
+    public void LoadPuzzleGame()
+    {
+        SceneManager.LoadScene("SlidingTilePuzzle");
+        Debug.Log("Loading PuzzleGame...");
+    }
+
+    public void LoadWaterState()
+    {
+        SceneManager.LoadScene("WaterStates");
+        Debug.Log("Loading WaterStates...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
